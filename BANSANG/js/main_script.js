6b8ceb409aab7b5a1b9e0a0f1ca3e46fdb9c02e2
Song_Saens <50483226+ssh9199@users.noreply.github.com>
2020-04-23 14:58:24 +0900

    BANSANG + navigation bar + scroll img + page img resize same as window

---
+++ BANSANG/js/main_script.js
@@ -0,0 +1,43 @@
+ jQuery(document).ready(function($) {
+
+     /* ------------ FitText Settings ------------ */
+     setTimeout(function() {
+         $('h1').fitText(1, {
+             minFontSize: '40px',
+             maxFontSize: '90px'
+         });
+     }, 100);
+
+
+     /* ------------ Smooth Scrolling ------------ */
+     $('.smoothscroll').on('click', function(e) {
+         e.preventDefault();
+
+         var target = this.hash,
+             $target = $(target);
+
+         $('html, body').stop().animate({
+             'scrollTop': $target.offset().top
+         }, 800, 'swing', function() {
+             window.location.hash = target;
+         });
+     });
+
+     var resizing_contents = ['header', '#page1', '#page2', '#page3'];
+
+
+     /* ------------ Height resize ------------ */
+     for(var i=0 ; i<resizing_contents.length ; ++i){
+         $(resizing_contents[i]).css({
+             'height': $(window).height()
+         });
+         $(window).on('resize', function() {
+             $(resizing_contents[i]).css({
+                 'height': $(window).height()
+             });
+             $('body').css({
+                 'width': $(window).width()
+             })
+         });
+     }
+ });
