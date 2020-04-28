bf21db8bd14813d130ee6976f4b6bafc89a287fe
Song_Saens <50483226+ssh9199@users.noreply.github.com>
2020-04-28 14:45:23 +0900

    scroll refactoring : touch, mouse click

--- BANSANG/js/pageScroll.js
+++ BANSANG/js/pageScroll.js
@@ -1,17 +1,27 @@
 var elm = ".box";
 var elmSelecter;
-$(".body").on("scroll touchmove mousewheel DOMMouseScroll", function(e){e.preventDefault();});
-$("#kakao").on("scroll touchmove mousewheel DOMMouseScroll", function(e){e.preventDefault();});
+$(".body").on("scroll touchmove mousewheel DOMMouseScroll", function(e) {
+    e.preventDefault();
+});
+$("#kakao").on("scroll touchmove mousewheel DOMMouseScroll", function(e) {
+    e.preventDefault();
+});
 
 
 window.onload = function() {
 
-    $(".body").on("scroll touchmove mousewheel DOMMouseScroll", function(e){e.preventDefault();});
-    $("#kakao").on("scroll touchmove mousewheel DOMMouseScroll", function(e){e.preventDefault();});
+    $(".body").on("scroll touchmove mousewheel DOMMouseScroll", function(e) {
+        e.preventDefault();
+    });
+    $("#kakao").on("scroll touchmove mousewheel DOMMouseScroll", function(e) {
+        e.preventDefault();
+    });
 
     var next;
     var first = false;
     var last = false;
+    var a_pos;
+    var a_speed;
     elm = ".box";
 
     $(elm).each(function(index) {
@@ -27,23 +37,49 @@ window.onload = function() {
             if (startY - endY > 50) { // 아래로
                 last = index > 4;
                 if ($(elmSelecter).next() != undefined) {
-                    next=1;
-                    if(!last) elmSelecter.find('.bgbg').fadeOut(600);
+                    next = elmSelecter.next();
+                    if (!last) elmSelecter.find('.bgbg').fadeOut(600);
                     else elmSelecter.find('.bgbg').fadeIn(600);
                     try {
                         moveTop = $(elmSelecter).next().offset().top;
                     } catch (event) {}
                 }
-            } else if (endY - startY > 50) {
+            } else if (endY - startY > 50) { // 위로
                 first = index < 2;
                 if ($(elmSelecter).prev() != undefined) {
-                    next=-1;
-                    if(!first) elmSelecter.find('.bgbg').fadeOut(600);
+                    next = elmSelecter.prev();
+                    if (!first) elmSelecter.find('.bgbg').fadeOut(600);
                     else elmSelecter.find('.bgbg').fadeIn(600);
                     try {
                         moveTop = $(elmSelecter).prev().offset().top;
                     } catch (event) {}
                 }
+            }
+            switch (next.children(0).attr('id')) {
+                case 'home':
+                    a_pos = {
+                        left: '40%'
+                    };
+                    a_speed = 1000;
+                    break;
+                case 'page1':
+                    a_pos = {
+                        left: '40%'
+                    };
+                    a_speed = 800;
+                    break;
+                case 'page2':
+                    a_pos = {
+                        left: '50%'
+                    };
+                    a_speed = 600;
+                    break;
+                case 'page3':
+                    a_pos = {
+                        left: '55%'
+                    };
+                    a_speed = 1000;
+                    break;
             }
 
             $("html,body").stop().animate({
@@ -51,10 +87,12 @@ window.onload = function() {
             }, {
                 duration: 900,
                 complete: function() {
-                    if(next == 1) elmSelecter.next().children(0).children(0).fadeIn(600);
-                    else if(next == -1) elmSelecter.prev().children(0).children(0).fadeIn(600);
-                    first = false; last = false;
-                    startY=0; endY=0;
+                    next.find('.bgbg').fadeIn(600);
+                    next.find('.bg-m').animate(a_pos, a_speed);
+                    first = false;
+                    last = false;
+                    startY = 0;
+                    endY = 0;
                 }
             });
         });
@@ -73,8 +111,8 @@ window.onload = function() {
             if (mstartY - mendY > 50) {
                 last = index > 4;
                 if ($(elmSelecter).next() != undefined) {
-                    next=1;
-                    if(!last) elmSelecter.find('.bgbg').fadeOut(600);
+                    next = elmSelecter.next();
+                    if (!last) elmSelecter.find('.bgbg').fadeOut(600);
                     else elmSelecter.find('.bgbg').fadeIn(600);
                     try {
                         moveTop = $(elmSelecter).next().offset().top;
@@ -83,13 +121,39 @@ window.onload = function() {
             } else if (mendY - mstartY > 50) {
                 first = index < 2;
                 if ($(elmSelecter).prev() != undefined) {
-                    next=-1;
-                    if(!first) elmSelecter.find('.bgbg').fadeOut(600);
+                    next = elmSelecter.prev();
+                    if (!first) elmSelecter.find('.bgbg').fadeOut(600);
                     else elmSelecter.find('.bgbg').fadeIn(600);
                     try {
                         moveTop = $(elmSelecter).prev().offset().top;
                     } catch (event) {}
                 }
+            }
+            switch (next.children(0).attr('id')) {
+                case 'home':
+                    a_pos = {
+                        left: '40%'
+                    };
+                    a_speed = 1000;
+                    break;
+                case 'page1':
+                    a_pos = {
+                        left: '40%'
+                    };
+                    a_speed = 800;
+                    break;
+                case 'page2':
+                    a_pos = {
+                        left: '50%'
+                    };
+                    a_speed = 600;
+                    break;
+                case 'page3':
+                    a_pos = {
+                        left: '55%'
+                    };
+                    a_speed = 1000;
+                    break;
             }
 
             $("html,body").stop().animate({
@@ -97,15 +161,17 @@ window.onload = function() {
             }, {
                 duration: 900,
                 complete: function() {
-                    if(next == 1) elmSelecter.next().children(0).children(0).fadeIn(600);
-                    else if(next == -1) elmSelecter.prev().children(0).children(0).fadeIn(600);
-                    first = false; last = false;
-                    startY=0; endY=0;
+                    next.find('.bgbg').fadeIn(600);
+                    next.find('.bg-m').animate(a_pos, a_speed);
+                    first = false;
+                    last = false;
+                    startY = 0;
+                    endY = 0;
                 }
             });
         });
 
-        $(this).on("scroll touchmove",function(e){
+        $(this).on("scroll touchmove", function(e) {
             e.preventDefault();
         });
 
@@ -128,30 +194,36 @@ window.onload = function() {
                 last = index > 4;
                 if ($(elmSelecter).next() != undefined) {
                     next = elmSelecter.next();
-                    if(!last) {
+                    if (!last) {
                         elmSelecter.find('.bgbg').fadeOut(600);
-                        elmSelecter.find('.bg-m').animate({left: "100%"},600);
-                    }
-                    else {
+                        elmSelecter.find('.bg-m').animate({
+                            left: "100%"
+                        }, 600);
+                    } else {
                         elmSelecter.find('.bgbg').fadeIn(600);
-                        elmSelecter.find('.bg-m').animate({left: "50%"},600);
+                        elmSelecter.find('.bg-m').animate({
+                            left: "50%"
+                        }, 600);
                     }
                     try {
                         moveTop = $(elmSelecter).next().offset().top;
                     } catch (e) {}
                 }
-            // 마우스휠을 아래에서 위로
-            } else if(delta >0) {
+                // 마우스휠을 아래에서 위로
+            } else if (delta > 0) {
                 first = index < 2;
                 if ($(elmSelecter).prev() != undefined) {
                     next = elmSelecter.prev();
-                    if(!first) {
+                    if (!first) {
                         elmSelecter.find('.bgbg').fadeOut(600);
-                        elmSelecter.find('.bg-m').animate({left: "100%"},600);
-                    }
-                    else {
+                        elmSelecter.find('.bg-m').animate({
+                            left: "100%"
+                        }, 600);
+                    } else {
                         elmSelecter.find('.bgbg').fadeIn(600);
-                        elmSelecter.find('.bg-m').animate({left: "50%"},600);
+                        elmSelecter.find('.bg-m').animate({
+                            left: "50%"
+                        }, 600);
                     }
                     try {
                         moveTop = $(elmSelecter).prev().offset().top;
@@ -159,24 +231,30 @@ window.onload = function() {
                 }
             }
 
-            var a_pos;
-            var a_speed;
 
-            switch(next.children(0).attr('id')) {
-                case 'home' :
-                    a_pos = {left: '40%'};
+            switch (next.children(0).attr('id')) {
+                case 'home':
+                    a_pos = {
+                        left: '40%'
+                    };
                     a_speed = 1000;
                     break;
-                case 'page1' :
-                    a_pos = {left: '40%'};
+                case 'page1':
+                    a_pos = {
+                        left: '40%'
+                    };
                     a_speed = 800;
                     break;
-                case 'page2' :
-                    a_pos = {left: '50%'};
+                case 'page2':
+                    a_pos = {
+                        left: '50%'
+                    };
                     a_speed = 600;
                     break;
-                case 'page3' :
-                    a_pos = {left: '55%'};
+                case 'page3':
+                    a_pos = {
+                        left: '55%'
+                    };
                     a_speed = 1000;
                     break;
             }
@@ -187,10 +265,11 @@ window.onload = function() {
                 duration: 900,
                 complete: function() {
                     next.find('.bgbg').fadeIn(600);
-                    next.find('.bg-m').animate( a_pos, a_speed);
-                    first = false; last = false;
-                    delta=0;
-                    next=0;
+                    next.find('.bg-m').animate(a_pos, a_speed);
+                    first = false;
+                    last = false;
+                    delta = 0;
+                    next = 0;
                 }
             });
         });
